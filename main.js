60031cdf8852e5d2af500d8fa4294359445b0611
Dirty Old Town <boloks70@gmail.com>
2024-09-27 23:06:01 +0200

    First step

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ document.addEventListener("mousedown", (event) => {
   event.preventDefault();
 });
 
-let display = document.querySelector("input");
-let keypad = document.querySelector(".calculation-container > input");
+let keypad = document.querySelector(".keypad");
+let display = document.querySelector(".calculation-container > input");
 let currentExpression = "";
 
+keypad.addEventListener("click", (e) => {
+  let target = e.target.closest(".number, .operator.visible");
+  if (!target) return;
+  let currentInput = target.textContent;
+  display.value += currentInput;
+});
 
 
